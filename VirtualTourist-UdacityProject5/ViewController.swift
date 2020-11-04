bc0f99fbaad0006ed5713c51e048d6ac4e560551
Cédric Morier-Roy <cmorierroy@gmail.com>
2020-11-04 11:34:57 -0600

    initial commit

---
+++ VirtualTourist-UdacityProject5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VirtualTourist-UdacityProject5
+//
+//  Created by Cédric Morier-Roy on 2020-11-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
